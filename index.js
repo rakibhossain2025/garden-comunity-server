cb95e8290b592b76be93fc518a60b3fb36262d32
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-05-21 18:24:33 +0600

    server connected

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT || 4710;
 app.use(cors())
-app.get('/', (req, res) => {
-  res.send("iam backend")
-})
-const user = {
-  name: 'rakib'
-  , sdf: 'rakib'
-  , ame: 'rakib'
+app.use(express.json())
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.Ass_User}:${process.env.Ass_Credential}@cluster0.hbdtmat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const gardenDataCollections = client.db("gardenTips").collection('tips')
+
+
+    app.get('/', (req, res) => {
+      console.log(res.send("iam here"))
+    })
+    app.post('/tips', async (req, res) => {
+      const tip = req.body;
+      const result = await gardenDataCollections.insertOne(tip)
+      res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally { }
 }
-app.get('/user', (req, res) => {
-  res.send(user)
-})
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`backend server is running on port is ${port}`)
 })
